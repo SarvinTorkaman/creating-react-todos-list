86da10424dd21929c0ba322cbbd9714826d01715
SarvinTorkaman <88194904+SarvinTorkaman@users.noreply.github.com>
2022-01-01 11:33:37 +0330

    adding the useEffect and useRef for form focus

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function TodoForm(props) {
   const [input, setInput] = useState("");
+  const inputElement = useRef(null);
+
+  useEffect(() => {
+    inputElement.current.focus();
+  });
+
   const handleChange = (e) => {
     setInput(e.target.value);
   };
@@ -25,6 +31,7 @@ function TodoForm(props) {
         name="text"
         className="todo-input"
         onChange={handleChange}
+        ref={inputElement}
       />
       <input type="submit" value="Add todo" className="todo-button" />
     </form>
